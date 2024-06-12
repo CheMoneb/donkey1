cb231c78402f802de2ee782e455a17bfaf817af9
Chervine Monashipour <chervine1999@gmail.com>
2024-06-12 18:56:45 +0200

    ajout de fichier (recommancer à zéro)

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+function barnav() {
+    var x = document.getElementById("liens");
+    if (x.style.display == "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
